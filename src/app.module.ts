7cb352e417814f73b434b14871701e20c04a5422
Sohail Raoufi <sohailraufi19@gmail.com>
2024-10-28 23:16:24 +0430

    Module for Prisma Client

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
