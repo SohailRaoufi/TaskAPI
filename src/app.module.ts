20f1a29d9c0374919c2fe630493f45f60180b7c8
Sohail Raoufi <sohailraufi19@gmail.com>
2024-11-01 12:03:56 +0430

    Auth Module Created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
 import { CategoryModule } from './category/category.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot({isGlobal:true}), TaskModule, CategoryModule]
+  imports: [PrismaModule, ConfigModule.forRoot({isGlobal:true}), TaskModule, CategoryModule, AuthModule]
 })
 export class AppModule {}
